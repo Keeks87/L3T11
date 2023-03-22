f31875e2be1a79422f2a7c73b41616fbf8a1ec19
Keenan <keenandup87@gmail.com>
2023-03-22 07:07:59 +0200

    update garden.py

--- garden.py
+++ garden.py
@@ -21,3 +21,38 @@ for sentence in gardenpathSentences:
     for ent in doc.ents:
         # Print the text of the entity and the entity label
         print(ent.text, ent.label_)
+
+print()
+print(spacy.explain("LOC"))
+print(spacy.explain("NORP"))
+
+'''
+Q-  At the bottom of your file, write a comment about two entities that you
+    looked up. For each entity answer the following questions:
+
+        What was the entity and its explanation that you looked up?
+
+        Did the entity make sense in terms of the word associated with it?
+
+A-  Two entities that I looked up are:
+
+    LOC
+
+        Explanation:
+        
+            Non-GPE locations, mountain ranges, bodies of water 
+        
+        Yes, the entity makes sense in terms of the words associated with it.
+        For example, in the sentence "John went hiking in the mountains," 
+        "mountains" would be categorized as a LOC entity.
+
+    NORP
+
+        Explanation:
+        
+            Nationalities or religious or political groups
+            
+        Yes, the entity makes sense in terms of the words associated with it.
+        For example, in the sentence "The Irish team won the game," 
+        "Irish" would be categorized as a NORP entity.
+'''
